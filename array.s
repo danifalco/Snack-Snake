141c193f69ee0cdb23be9dad46c4c69d1c5b35b9
danifalco <72247549+danifalco@users.noreply.github.com>
2023-12-07 15:29:38 +0000

    Added return tail funcs for x and y

--- array.s
+++ array.s
@@ -2,12 +2,13 @@
 
 extrn	x_pos, y_pos
     
-global	array_setup
+global	array_setup, snek_not_grow, snek_grow
+global	snek_len,
     
 psect	udata_acs
 snek_len:	ds  1	    ; Reserve 1 byte for length of snake
 loop_cnt:	ds  1	    ; Reserve 1 byte for a loop counter
-x_poos:		ds  1	    ; TODO DELETE
+
 psect	udata_bank1	    ; We store the x-values array in bank 1
 x_Arr:		ds  0xFF    ; We can have a snake of length up to 256
 psect	udata_bank2	    ; Store the y-values in bank 2
@@ -46,47 +47,49 @@ snek_not_grow:
     ; Shifts every value of the array to the right, puts the new value of x,y in
     ; the first array intexes. Does NOT increase snek_len. This will probably 
     ; Run into trouble when snek_len>255 but hey
+    
     movf    snek_len, W, A  ; Load snek length into W
     addwf   FSR0L, A	    ; Select the nth position of the list
     movlw   0x0
     movwf   loop_cnt, A
-    snek_grow_loop_x:
+    snek_not_grow_loop_x:
 	movf    INDF0, W, A	; Move the value of n to WREG
 	incf    FSR0L, A	; Select position n+1
 	movwf   INDF0, A	; Move WREG to n+1
 	decf    FSR0L, A	; Move pointer back to n
 	movf	snek_len, W, A	; Move snek len to W to compare in next inst
 	cpfslt	loop_cnt, A ; Break loop once snek_len iterations are performed
-	bra	in_new_x
+	bra	_in_new_x
 	decf    FSR0L, A	; Move pointer to n-1 (if loop hasn't terminated)
 	incf	loop_cnt, A	; Increment the counter
-	bra	snek_grow_loop_x
-    in_new_x:
+	bra	snek_not_grow_loop_x
+    _in_new_x:
 	lfsr	0, x_Arr	; Set FRS0 back to back to the first point
 	movff	x_pos, INDF0	
-	bra snek_grow_y		; Notice how we can't increase length yet, first
+	bra snek_not_grow_y		; Notice how we can't increase length yet, first
 				; deal with the y array
 	
-snek_grow_y:			; Same thing as x, but with y
+snek_not_grow_y:			; Same thing as x, but with y
     movf    snek_len, W, A 
     addwf   FSR1L, A	    
     movlw   0x0
     movwf   loop_cnt, A
-    snek_grow_loop_y:
+    snek_not_grow_loop_y:
 	movf    INDF1, W, A
 	incf    FSR1L, A	
 	movwf   INDF1, A	
 	decf    FSR1L, A	
 	movf	snek_len, W, A
 	cpfslt	loop_cnt, A 
-	bra	in_new_y
+	bra	_in_new_y
 	decf    FSR1L, A	
 	incf	loop_cnt, A	
-	bra	snek_grow_loop_y
-    in_new_y:
+	bra	snek_not_grow_loop_y
+    _in_new_y:
 	lfsr	0, y_Arr	
 	movff	y_pos, INDF1
 	return			    ; Notice we do not increase snek_len
+	
 	
 snek_grow:
     ; Shifts every value of the array to the right, puts the new value of x,y in
@@ -133,5 +136,15 @@ snek_grow_y:			; Same thing as x, but with y
 	movff	y_pos, INDF1
 	incf	snek_len, A	; This time, increase snek_len
 	return
+	
+ret_x_tail:	; Returns the x-position of the tali in WREG
+    movf    snek_len, W, A	   
+    addwf   FSR0L, A	    ; Point FSR0 to last element (i.e. tail)
+    movf    INDF0, W, A	    ; Move tail value to WREG
+    lfsr    0, x_Arr	    ; Reset FSR0 to point to x_Arr
     
-
+ret_y_tail:	; Returns the y-position of the tail in WREG
+    movf    snek_len, W, A	   
+    addwf   FSR1L, A	    ; Point FSR1 to last element (i.e. tail)
+    movf    INDF1, W, A	    ; Move tail value to WREG
+    lfsr    1, y_Arr	    ; Reset FSR1 to point to y_Arr
