6df7bc485a1577b3cdac84d89168d83c0aa12073
Daniel Weronski Falcó <72247549+danifalco@users.noreply.github.com>
2023-12-11 15:49:24 +0000

    Food spawns and snake can eat food. Buggy though

--- propagation.s
+++ propagation.s
@@ -4,9 +4,12 @@ global	propagation_setup, init_position, propagate, dsp_pos_head
 global	mv_right, mv_left, mv_up, mv_down
 global	remain, temp, counter, x_pos, y_pos
 global	x_row_inst, y_col_inst, x_tail_old, y_tail_old, remain, x_temp
+global	Y_POS_COMMAND, X_POS_COMMAND, dirty_read 
 
 extrn	GLCD_Send_I, GLCD_Send_D, GLCD_Read, vert_line
 extrn	ret_x_tail, ret_y_tail, ret_x_head, ret_y_head, snek_grow, snek_not_grow
+extrn	check_if_in_snek
+extrn	food_x, food_y, spawn_food
     
 psect udata_acs	    ; named variables in access RAM
 
@@ -23,6 +26,7 @@ temp:	    ds	1   ; reserve 1 byte for a versatile temp variable
 counter:    ds	1   ; reserve 1 byte for a counter variable
 x_tail_old: ds	1   ; reserve 1 byte for the previous tail x-position
 y_tail_old: ds	1   ; reserve 1 byte for the previous tail y-position
+grow_bool:  ds  1   ; reserve 1 byte for growth condition: 1=grow, 0=not
     
 one:	    ds	1
 two:	    ds  1
@@ -59,7 +63,7 @@ init_position:
     call    GLCD_Send_I	    ; Select y=0 column
     movlw   10111000B
     call    GLCD_Send_I	    ; Select x=0 column
-    movlw   00001110B
+    movlw   11111110B
     call    GLCD_Send_D
     movlw   01000000B
     call    GLCD_Send_I	    ; Select y=0 column
@@ -70,6 +74,7 @@ propagate:  ; Propagate: 1 - Right, 2 - Left, 3 - Up, 4 - Down
     
     ;movff    x_pos, x_prev, A
     ;movff    y_pos, y_prev, A
+    clrf    grow_bool, A
     movwf   temp, A		    ; Hold WREG in temp since it will change
     clrf    remain, A
     call    ret_x_head
@@ -101,6 +106,17 @@ down:
     bra	    dsp_pos_head
 
 dsp_pos_head:  ; Figure out the row and column of the snake head
+    call    check_if_in_snek	; First of all check if snek ate itself
+    movf    food_x, W, A	
+    cpfseq  x_pos, A		; Compare if food_x and x_pos are the same
+    bra	    continue
+    movf    food_y, W, A	; If so check for y
+    cpfseq  y_pos, A
+    bra	    continue
+    movlw   1
+    movwf   grow_bool, A	; If it is in y, set grow_bool to 1 (true)
+
+    continue:
     call    ret_x_tail
     movwf   x_tail_old, A	; Store the old tail position into its var
     call    ret_y_tail
@@ -164,10 +180,11 @@ end_display:
     addlw   Y_POS_COMMAND	; Sum with the command vlaue and store in W
     call    GLCD_Send_I		; Reset y position since GLCD_Read +1'ed it
     
-    
-    bra	    dsp_pos_tail	; TODO Implement some logic to check whether
-				; snek grow or snek not grow
+    movlw   1
+    cpfseq  grow_bool, A	; Check if snake has grown
+    bra	    dsp_pos_tail	; Case: Snek not grow
     call    snek_grow		; We haven't deleted the last pixel so snek grow
+    call    spawn_food
     return    
    
 dsp_pos_tail:	; Finds the row, col of the tail value (
@@ -287,6 +304,10 @@ dirty_read:
     movf    y_col_inst, W, A
     call    GLCD_Send_I		; Select y column
     movf    x_row_inst, W, A
+    call    GLCD_Send_I		; Select x column
+    call    GLCD_Read		; Read contents from screen in selected area
+    
+    return
     call    GLCD_Send_I		; Select x column
     call    GLCD_Read		; Read contents from screen in selected area
     
